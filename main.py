880e7653fae3e179ce0fa3ffc7ea77f4f8ded89c
陈嘉健 <550728110@qq.com>
2019-06-22 23:14:17 +0800

    基本实现所有功能 todo: 清空表格 todo: 删除添加的文件

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
-from re import search
 from tkinter import *
 from tkinter import filedialog, messagebox, ttk
-from xlrd import open_workbook, XLRDError, xldate_as_tuple
+
+from xlrd import open_workbook, XLRDError
+
+
+# 温馨提示
+# 1.请勿在文件名中添加'.'
 
 
 class MyGUI:
     def __init__(self):
         self.cur_pos = None
         self.cur_path = ""  # 当前路径
-        self.table_name_list = []  # 表格中现有的文件路径列表
+        self.table_name_list = []  # 表格中现有的文件路径列表（含后缀名）
         # display list
         self.table_name_list0 = []  # 表格中现有的旧文件名列表
         self.table_name_list1 = []  # 表格中现有的新文件名列表
@@ -32,7 +36,7 @@ class MyGUI:
 
         self.open_files_button = Button(self.init_window, text="选择文件", command=self.open_files)
         self.open_xls_button = Button(self.init_window, text="选择模板", command=self.open_template)
-        self.confirm_button = Button(self.init_window, text="确认更改")
+        self.confirm_button = Button(self.init_window, text="确认更改", command=self.check_new_name)
 
         self.set_init_window()
         self.init_window.mainloop()
@@ -55,9 +59,9 @@ class MyGUI:
         self.tree_view.heading("2", text="新文件名")
         self.tree_view.heading("3", text="类型")
 
-        self.open_files_button.place(relx=0.8, rely=0.2, relwidth=0.15, relheight=0.15)
-        self.open_xls_button.place(relx=0.8, rely=0.5, relwidth=0.15, relheight=0.15)
-        self.confirm_button.place(relx=0.8, rely=0.8, relwidth=0.15, relheight=0.15)
+        self.open_files_button.place(relx=0.8, rely=0.2, relwidth=0.15, relheight=0.18)
+        self.open_xls_button.place(relx=0.8, rely=0.49, relwidth=0.15, relheight=0.18)
+        self.confirm_button.place(relx=0.8, rely=0.77, relwidth=0.15, relheight=0.18)
 
         self.edit_tips_label.place(relx=0.05, rely=0.85, relwidth=0.3)
 
@@ -108,8 +112,8 @@ class MyGUI:
 
     def set_file_location(self, file_path):
         (file_path, temp_filename) = os.path.split(file_path)
-        self.cur_path = file_path
-        self.cur_pos_label.configure(text=file_path)
+        self.cur_path = file_path + '/'
+        self.cur_pos_label.configure(text=self.cur_path)
 
     def is_same_location(self, file_path):
         (file_path, temp_filename) = os.path.split(file_path)
@@ -145,14 +149,28 @@ class MyGUI:
         else:
             self.name_reflect_dict = temp_dict
             print(self.name_reflect_dict)
-            # self.update_table_by_dict()
+            self.update_table_by_dict()
 
+    # 使用文件名映射更新表格（全局）
     def update_table_by_dict(self):
         for i in range(len(self.table_name_list0)):
             if self.name_reflect_dict.keys().__contains__(self.table_name_list0[i]):
-                self.table_name_list1.append(self.name_reflect_dict[self.table_name_list0[i]])
+                temp_name = self.name_reflect_dict[self.table_name_list0[i]]
+                if not is_name_legal(temp_name):
+                    messagebox.showwarning("新文件名格式有误", "新文件名有误：" + temp_name + "\n合法文件名不应存在\\ / \" * : ? < > | 等字符")
+                    self.table_name_list1.clear()
+                    return
+                else:
+                    self.table_name_list1.append(temp_name)
             else:
                 self.table_name_list1.append("")
+        # 更新新文件名列表后插入表格
+        row_str = self.tree_view.get_children()
+        for cur_row in row_str:
+            rn = int(str(cur_row).replace('I', ''))
+            print(cur_row)
+            self.tree_view.set(cur_row, column='#2', value=self.table_name_list1[rn - 1])
+        self.set_button_state(1)
 
     # 使按钮失效，无法使用
     def set_button_state(self, i):
@@ -172,23 +190,69 @@ class MyGUI:
 
     def set_cell_value(self, event):  # 双击进入编辑状态
         for item in self.tree_view.selection():
+            print("set_cell_value" + item)
             item_text = self.tree_view.item(item, "values")
-            print(item_text[0:2])  # 输出所选行的值
+            print(item_text[0:3])  # 输出所选行的值
+
         column = self.tree_view.identify_column(event.x)  # 列
-        row = self.tree_view.identify_row(event.y)  # 行
-        entry_edit = Text(self.init_window, width=20, height=1)
-        entry_edit.place(relx=0.35, rely=0.85, relwidth=0.3, relheight=0.08)
+        row = self.tree_view.identify_row(event.y)
+        cn = int(str(column).replace('#', ''))
+        rn = int(str(row).replace('I', ''))
+        print(rn)
+        if cn == 2:
+            entry_edit = Text(self.init_window, width=20, height=1)
+            entry_edit.place(relx=0.35, rely=0.85, relwidth=0.3, relheight=0.08)
 
-        def save_edit():
-            try:
-                self.tree_view.set(item, column=column, value=entry_edit.get(0.0, "end"))
-            except NameError:
-                messagebox.showwarning("文件未添加", "请先添加文件后修改")
-            entry_edit.destroy()
-            ok_btn.destroy()
+            def save_edit():
+                new_name = entry_edit.get(1.0, "end")
+                print("new name:" + new_name)
+                if not is_name_legal(new_name):
+                    messagebox.showwarning("新文件名格式有误", "新文件名有误：" + new_name + "\n合法文件名不应存在\\ / \" * : ? < > | 等字符")
+                    entry_edit.delete(0.0, "end")
+                else:
+                    try:
+                        self.tree_view.set(item, column=column, value=new_name)
+                        self.table_name_list1[rn - 1] = new_name  # 刷新新文件名的列表
+                    except NameError:
+                        messagebox.showwarning("文件未添加", "请先添加文件后修改")
+                    entry_edit.destroy()
+                    ok_btn.destroy()
 
-        ok_btn = ttk.Button(self.init_window, text='OK', command=save_edit)
-        ok_btn.place(relx=0.65, rely=0.85, relwidth=0.1, relheight=0.08)
+            ok_btn = ttk.Button(self.init_window, text='OK', command=save_edit)
+            ok_btn.place(relx=0.65, rely=0.85, relwidth=0.1, relheight=0.08)
+
+    def check_new_name(self):
+        print("def check_new_name")
+        for new_name in self.table_name_list1:
+            if len(new_name) == 0:
+                a = messagebox.askokcancel('空文件名提示', '个别文件无新文件名，是否继续更名？')
+                if a:
+                    self.confirm_rename()
+                else:
+                    return
+        self.confirm_rename()
+
+    def confirm_rename(self):
+        print("def confirm_rename")
+        print(self.cur_path)
+        os.chdir(self.cur_path)
+        for i in range(len(self.table_name_list0)):
+            if not len(self.table_name_list1[i]) == 0:
+                name = self.table_name_list[i]
+                (name, ext) = os.path.splitext(name)
+                old_name = self.table_name_list0[i] + ext
+                new_name = self.table_name_list1[i] + ext
+                print("old_name" + old_name)
+                print("new_name" + new_name)
+                try:
+                    os.rename(old_name, new_name)
+                except FileNotFoundError:
+                    messagebox.showwarning("无法找到文件", "系统找不到指定的文件" + old_name)
+                    return
+
+        messagebox.showinfo("文件重命名成功", "所有文件名修改成功")
+        # todo: 清空表格
+        # todo: 删除添加的文件
 
 
 # 数据类
@@ -214,11 +278,18 @@ class ExcelMaster:
         name0_list = self.table.col_values(i, start_rowx=1, end_rowx=None)
         j = self.col_index('新文件名')
         name1_list = self.table.col_values(j, start_rowx=1, end_rowx=None)
-        print(self.table.col_types(i, start_rowx=1, end_rowx=None))
-        print(self.table.col_types(j, start_rowx=1, end_rowx=None))
         if len(name0_list) != len(name1_list) or len(name0_list) == 0:
             return None
         else:
+            type0_list = self.table.col_types(i, start_rowx=1, end_rowx=None)
+            type1_list = self.table.col_types(j, start_rowx=1, end_rowx=None)
+            # 净化识别为数字的数据
+            for i in range(len(type0_list)):
+                if type0_list[i] == 2:
+                    name0_list[i] = str(name0_list[i]).split(".")[0]
+            for i in range(len(type1_list)):
+                if type1_list[i] == 2:
+                    name1_list[i] = str(name1_list[i]).split(".")[0]
             name_dict = dict()
             for i in range(len(name0_list)):
                 name_dict[str(name0_list[i])] = str(name1_list[i])
@@ -238,7 +309,6 @@ class ExcelMaster:
 def is_name_legal(text):
     if len(text) == 0:
         return True
-
     # 正则表达式判断特殊字符
     if not re.search(u'[\\\\/:*?\"<>|]', text):
         return True
